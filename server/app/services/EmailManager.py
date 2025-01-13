293091a7c0250c2f9a109adcffe35982b73625ce
Dinhh-Chan <dinhtran29092005@gmail.com>
2025-01-13 16:19:58 +0700

    update service email

--- server/app/services/EmailManager.py
+++ server/app/services/EmailManager.py
@@ -105,7 +105,86 @@ class Update_EmailManager(EmailManager):
         with smtplib.SMTP_SSL(self.HOST, self.PORT) as sv:
             sv.login(self.__admin_account, self.__admin_password)
             sv.sendmail(self.__admin_account, recipients, msg.as_string())
-            
+class ClassSchedule_EmailManager(EmailManager):
+    def __init__(self):
+        super().__init__()
+        self.set_template()
+    
+    def set_template(self):
+        self.template = get_class_schedule_template()
+        
+    def announce(self, email, ten_lop_hoc, thoi_gian, giang_vien, dia_diem, ghi_chu):
+        recipients = [self.__admin_account, email]
+        msg = MIMEMultipart('alternative')
+        msg["Subject"] = "THÔNG BÁO LỊCH HỌC MỚI"
+        msg["From"] = self.__admin_account
+        msg["To"] = ", ".join(recipients)
+        html_content = self.template.format(
+            ten_lop_hoc,
+            thoi_gian,
+            giang_vien,
+            dia_diem,
+            ghi_chu
+        )
+        msg.attach(MIMEText(html_content, 'html'))        
+        with smtplib.SMTP_SSL(self.HOST, self.PORT) as sv:
+            sv.login(self.__admin_account, self.__admin_password)
+            sv.sendmail(self.__admin_account, recipients, msg.as_string())
+
+class TeachingSchedule_EmailManager(EmailManager):
+    def __init__(self):
+        super().__init__()
+        self.set_template()
+    
+    def set_template(self):
+        self.template = get_teaching_schedule_template()
+        
+    def announce(self, email, ten_giang_vien, khoa_hoc, thoi_gian, dia_diem, ghi_chu):
+        recipients = [self.__admin_account, email]
+        msg = MIMEMultipart('alternative')
+        msg["Subject"] = "THÔNG BÁO LỊCH GIẢNG DẠY MỚI"
+        msg["From"] = self.__admin_account
+        msg["To"] = ", ".join(recipients)
+        html_content = self.template.format(
+            ten_giang_vien,
+            khoa_hoc,
+            thoi_gian,
+            dia_diem,
+            ghi_chu
+        )
+        msg.attach(MIMEText(html_content, 'html'))        
+        with smtplib.SMTP_SSL(self.HOST, self.PORT) as sv:
+            sv.login(self.__admin_account, self.__admin_password)
+            sv.sendmail(self.__admin_account, recipients, msg.as_string())
+
+class EventNotification_EmailManager(EmailManager):
+    def __init__(self):
+        super().__init__()
+        self.set_template()
+    
+    def set_template(self):
+        self.template = get_event_notification_template()
+        
+    def announce(self, email, ten_su_kien, ngay, thoi_gian, dia_diem, mo_ta):
+        recipients = [self.__admin_account, email]
+        msg = MIMEMultipart('alternative')
+        msg["Subject"] = "THÔNG BÁO SỰ KIỆN MỚI"
+        msg["From"] = self.__admin_account
+        msg["To"] = ", ".join(recipients)
+        html_content = self.template.format(
+            ten_su_kien,
+            ngay,
+            thoi_gian,
+            dia_diem,
+            mo_ta
+        )
+        msg.attach(MIMEText(html_content, 'html'))        
+        with smtplib.SMTP_SSL(self.HOST, self.PORT) as sv:
+            sv.login(self.__admin_account, self.__admin_password)
+            sv.sendmail(self.__admin_account, recipients, msg.as_string())
+class_schedule_email_manager = ClassSchedule_EmailManager()
+teaching_schedule_email_manager = TeachingSchedule_EmailManager()
+event_notification_email_manager = EventNotification_EmailManager()
 create_email_manager = Create_EmailManager()
 cancel_email_manager = Cancel_EmailManager()
 update_email_manager = Update_EmailManager()
